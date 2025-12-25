e9d4992708db590e51ccd3ba18eba7df032f1daf
Adedamola Maxwell <promcode01@gmail.com>
2025-12-25 23:33:56 +0100

    Implement backend auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
-  schema: "./src/db/schema.ts",
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export default defineConfig({
+  schema: "./src/lib/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
